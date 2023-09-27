ae09d16312616eca9aa6b3a19c9a8a26c90dd1b8
Merc5Dark <31131794siya@gmail.com>
2023-09-27 13:55:02 +0200

    variables

--- main.js
+++ main.js
@@ -4,3 +4,16 @@
 // console.error('error');
 // console.warning('warning');
 
+// Variables let and const
+// let age = 30;
+// age = 31;
+
+// const age2 = 24;
+
+// console.log(age);
+// console.log(age2);
+// const is more safer and secure
+
+// Data types
+// String Numbers Boolean Null Undefined 
+
