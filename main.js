32ad6e712cde9ba6698efd8e0ce64d4136c33aff
Merc5Dark <31131794siya@gmail.com>
2023-09-27 15:20:39 +0200

    arrary tricks

---
+++ main.js
@@ -0,0 +1,36 @@
+// // Object literals
+
+// const person = {
+//     firstName: 'JavaScript',
+//     lastName: 'React',
+//     age: 24,
+//     hobbies: ['games', 'coding', 'skating'],
+//     address: {
+//         street: '50 50 Street',
+//         city: '50 50',
+//         state: 'MAS'
+//     }
+// }
+
+// console.log(person);
+
+
+const todos = [
+    {
+        id: 1,
+        text: 'Master JS',
+        isComplete: true
+    },
+    {
+        id: 2,
+        text: 'Learn Framework',
+        isComplete: true
+    },
+    {
+        id: 3,
+        text: 'Debbug',
+        isComplete: false
+    }
+]
+
+console.log(todos);
