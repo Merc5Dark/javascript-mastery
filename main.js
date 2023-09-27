6adb9103d3dc831feebe4e9943830232f70c8191
Merc5Dark <31131794siya@gmail.com>
2023-09-27 14:43:44 +0200

    data types

--- main.js
+++ main.js
@@ -17,3 +17,22 @@
 // Data types
 // String Numbers Boolean Null Undefined 
 
+// const name = 'Merc';
+// const age = 24;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof rating);
+// console.log(typeof isCool);
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(typeof z);
+
+const name = 'JavaScript';
+const age = 24;
+
